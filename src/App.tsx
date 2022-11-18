cfee3ee54569854bce5301a04b17caa0cd588a45
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 16:17:48 +0900

    add file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { createTheme } from '@material-ui/core'
 import { ThemeProvider as MuiThemProvider } from '@material-ui/core'
 import { indigo } from '@material-ui/core/colors'
-import logo from './logo.svg'
-import './App.css'
 
 const theme = createTheme({
   palette: {
@@ -18,24 +16,7 @@ const theme = createTheme({
 })
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <MuiThemProvider theme={theme}></MuiThemProvider>
 }
 
 export default App
