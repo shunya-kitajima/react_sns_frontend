cc02e47ac141dc459fcc16c645357ee29557afc8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 21:57:12 +0900

    import modules

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { withCookies } from 'react-cookie'
+import axios from 'axios'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -6,6 +8,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import { CircularProgress } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -28,6 +31,8 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Login: React.FC = () => {
+  const classes = useStyles()
+
   return <div></div>
 }
 
