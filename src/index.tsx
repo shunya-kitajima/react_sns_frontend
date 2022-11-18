389befc5e5ec181cf58e7987f1d22c2f08322893
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 21:35:42 +0900

    import CookiesProvider and Login component

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
 import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
+import Login from './components/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <CookiesProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </CookiesProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
