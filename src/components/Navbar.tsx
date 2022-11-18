2b33c1f9de9e46c3490bec3c2b3f60cc3a05a31e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 16:27:58 +0900

    import modules

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import Badge from '@material-ui/core/Badge'
+import { FiLogOut } from 'react-icons/fi'
+import { withCookies } from 'react-cookie'
+
+const Navbar: React.FC = () => {
+  return <div></div>
+}
+
+export default Navbar
