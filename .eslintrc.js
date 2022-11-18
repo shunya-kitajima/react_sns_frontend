7ca28733185c1420a1159800a84b4d464ed38705
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 16:02:35 +0900

    modify rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     es2021: true
   },
   extends: [
+    'next/core-web-vitals',
     'plugin:react/recommended',
     'standard-with-typescript'
   ],
   overrides: [
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
