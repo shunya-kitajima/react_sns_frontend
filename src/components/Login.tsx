7d1bf33a2986d8da58ef2a5e6cffdbef497541a0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-18 21:53:27 +0900

    import moduels

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
 const Login: React.FC = () => {
   return <div></div>
