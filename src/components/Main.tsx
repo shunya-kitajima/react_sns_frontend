38beddf0083cd3e7e82e167c47992096c0f8d333
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-21 18:12:13 +0900

    import modules

---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import { ApiContext } from '../context/ApiContext'
+import Grid from '@material-ui/core/Grid'
+import { GoMail } from 'react-icons/go'
+import { BsFillPeopleFill } from 'react-icons/bs'
+
+const Main: React.FC = () => {
+  return <Grid>Main</Grid>
+}
+
+export default Main
