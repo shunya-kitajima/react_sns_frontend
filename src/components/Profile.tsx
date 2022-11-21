2d1f08ed3c7b18e1deb9fa6459f9f71bbe0abe1c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-21 18:35:56 +0900

    import modules

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { ApiContext } from '../context/ApiContext'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const Profile: React.FC = () => {
+  return <div>Profile</div>
+}
+
+export default Profile
