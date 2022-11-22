ba3d35a770bb0fc005ca96f7e237426d597482b2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-22 11:31:51 +0900

    display profiles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,12 +3,30 @@ import { ApiContext } from '../context/ApiContext'
 import Grid from '@material-ui/core/Grid'
 import { GoMail } from 'react-icons/go'
 import { BsFillPeopleFill } from 'react-icons/bs'
+import UserProfile from './Profile'
 
 const Main: React.FC = () => {
+  const { profile, profiles, friendRequestList, allFriendRequestList, inbox } =
+    useContext(ApiContext)
+  const filteredProfiles = profiles.filter((prof) => prof.id !== profile.id)
+  const filteredRequests = allFriendRequestList.filter(
+    (request) =>
+      profile.userPro === request.askFrom || profile.userPro === request.askTo
+  )
+  const profileList =
+    filteredProfiles.length !== 0 &&
+    filteredProfiles.map((prof) => (
+      <UserProfile
+        key={prof.id}
+        profileData={prof}
+        requestDataArr={filteredRequests}
+      />
+    ))
+
   return (
     <Grid container>
       <Grid item xs={4}>
-        <div className="app-profiles"></div>
+        <div className="app-profiles">{profileList}</div>
       </Grid>
       <Grid item xs={4}>
         <div className="app-details"></div>
