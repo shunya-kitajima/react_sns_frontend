2699325049cbb06874bfc168a7e780d06e11b7ad
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-22 20:17:42 +0900

    modify

--- src/index.tsx
+++ src/index.tsx
@@ -9,16 +9,16 @@ import Login from './components/Login'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CookiesProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/profiles" element={<App />} />
-        </Routes>
-      </CookiesProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <CookiesProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/profiles" element={<App />} />
+      </Routes>
+    </CookiesProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
