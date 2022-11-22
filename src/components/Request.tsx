68ee8bfc60108a70e788aece9adf4d86dacca50a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-22 16:20:19 +0900

    import modules

---
+++ src/components/Request.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useContext } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import { ReMailAddLine } from 'react-icons/ri'
+import { IoIosSend, IoMdClose } from 'react-icons/io'
+import Modal from 'react-modal'
+import { ApiContext } from '../context/ApiContext'
+
+const Request: React.FC = () => {
+  return <div>Request</div>
+}
+
+export default Request
