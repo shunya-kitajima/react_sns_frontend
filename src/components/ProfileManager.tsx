70ad1c8146d6cb63b35980fd84d22fd3fcd11cb8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-22 11:50:11 +0900

    create useStyles

--- src/components/ProfileManager.tsx
+++ src/components/ProfileManager.tsx
@@ -7,6 +7,41 @@ import { MdAddAPhoto } from 'react-icons/md'
 import { FaUserEdit } from 'react-icons/fa'
 import ApiContext from '../context/ApiContext'
 
+const useStyles = makeStyles((theme) => ({
+  profile: {
+    '& .image-wrapper': {
+      textAlign: 'center',
+      position: 'relative',
+      '& button': {
+        position: 'absolute',
+        top: '80%',
+        left: '70%',
+      },
+      margin: 6,
+    },
+    '& .profile-img': {
+      width: 150,
+      height: 150,
+      objectFit: 'cover',
+      maxwidth: '100%',
+      borderRadius: '50%',
+      backgroundColor: 'white',
+    },
+    '& .profile-details': {
+      textAlign: 'center',
+      '& span, svg': {
+        verticalAlign: 'middle',
+        color: 'rightgray',
+        fontFamily: '"Comic Neue", cursive',
+      },
+    },
+    '& hr': {
+      border: 'none',
+      margin: '0 0 7px 0',
+    },
+  },
+}))
+
 const ProfileManager: React.FC = () => {
   return <div>ProfileManager</div>
 }
