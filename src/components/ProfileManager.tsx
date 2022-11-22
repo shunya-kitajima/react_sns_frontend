311e5736583849a64e0b947dcb93a97dafc77049
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-22 11:41:21 +0900

    import modules

---
+++ src/components/ProfileManager.tsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import LocationOn from '@material-ui/icons/LocationOn'
+import { IconButton } from '@material-ui/core'
+import { BsPersonCheckFill, BsPersonPlus, BsTrash } from 'react-icons/bs'
+import { MdAddAPhoto } from 'react-icons/md'
+import { FaUserEdit } from 'react-icons/fa'
+import ApiContext from '../context/ApiContext'
+
+const ProfileManager: React.FC = () => {
+  return <div>ProfileManager</div>
+}
+
+export default ProfileManager
